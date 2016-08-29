c8341ff0df5fb132eeb32a25371238ff83aef312
Ikanant <jonathan.hdez92@gmail.com>
2016-08-29 10:12:41 -0400

    Fixed custom filter & added new angular favicon

--- Demo_02_App/app/js/filters.js
+++ Demo_02_App/app/js/filters.js
@@ -2,7 +2,6 @@
 
 eventsApp.filter('durations', function(){
   return function (duration){
-    console.log(duration[0].duration + ":00 hours");
-    return duration[0].duration + ":00 hours";
+    return duration+":00 hrs";
   }
 });
