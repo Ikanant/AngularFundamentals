5e1eea3a3c0b772e2637cd4494eb33c741fbc30a
Ikanant <jonathan.hdez92@gmail.com>
2016-08-30 11:44:49 -0400

    Create first service to retrieve data

--- Demo_02_App/app/js/controllers/EventController.js
+++ Demo_02_App/app/js/controllers/EventController.js
@@ -1,47 +1,11 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope) {
+    function EventController($scope, eventData) {
 
         $scope.sortorder = 'name'
 
-        $scope.event = {
-            name: 'Angular Test',
-            date: '01/01/1992',
-            time: '10:30 am',
-            location: {
-                address: '1234 NW 56 Ave',
-                city: 'Miami',
-                state: 'FL'
-            },
-            imageUrl: 'img/angularjs-logo.png',
-            sessions: [
-                {
-                    name:'React-Native',
-                    creatorName:'Horacio Delgado',
-                    duration:3,
-                    level:'Advanced',
-                    abstract:'Cool class about React-Native',
-                    upVoteCount: 4
-                },
-                {
-                    name:'ASP.NET',
-                    creatorName:'Anibal Castineyra',
-                    duration:4,
-                    level:'Intermediate',
-                    abstract:'ASP.NET Core class',
-                    upVoteCount: 2
-                },
-                {
-                    name:'Golang',
-                    creatorName:'Jonathan Hernandez',
-                    duration:1,
-                    level:'Beginner',
-                    abstract:'Golang Basic class',
-                    upVoteCount: 3
-                }
-            ]
-        }
+        $scope.event = eventData.event;
 
         $scope.upVoteSession = function upVoteSession(session){
                 session.upVoteCount++;
