f8a335407e6f103a3901beb88c018decf626b99e
Ikanant <jonathan.hdez92@gmail.com>
2016-08-29 22:15:43 -0400

    Added series of Validation

--- Demo_02_App/app/js/controllers/EditEventController.js
+++ Demo_02_App/app/js/controllers/EditEventController.js
@@ -2,8 +2,10 @@
 
 eventsApp.controller('EditEventController',
     function EditEventController($scope) {
-        $scope.saveEvent = function(event){
-          window.alert('Event: ' + event.name + ' has been saved.');
+        $scope.saveEvent = function(event, newEventForm){
+          if(newEventForm.$valid){
+            window.alert('Event: ' + event.name + ' has been saved.');
+          }
         };
 
         $scope.cancelEvent = function() {
