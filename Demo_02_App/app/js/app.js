b889ee7f6d16709cb814c1c0f659bf18be9389ab
Ikanant <jonathan.hdez92@gmail.com>
2016-08-31 18:35:12 -0400

    first route !!! awesome

--- Demo_02_App/app/js/app.js
+++ Demo_02_App/app/js/app.js
@@ -1,4 +1,11 @@
 'use strict';
 
 // An empty array will tell Angular that we are not depending on any other module
-var eventsApp = angular.module('eventsApp', ['ngSanitize', 'ngResource']);
+var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
+  .config(function($routeProvider){
+    $routeProvider.when('/newEvent',
+      {
+        templateUrl: 'templates/NewEvent.html',
+        controller: 'EditEventController'
+      });
+  });
