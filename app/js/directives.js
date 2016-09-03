7a4897dabd2969df3440cecad90cc6b3d075509f
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2016-09-03 01:35:12 -0500

    Edited .gitignore & allowed custom directive to be accessed through an Element

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,6 +2,7 @@
 
 eventsApp.directive('myDirective', function($compile){
   return {
+    restrict: 'E',
     link: function (scope, element, attrs, controller) {
       var markup = '<input type="text" name="apple" ng-model="test"/> <h1>{{test}}</h1>';
       angular.element(element).html($compile(markup)(scope));
