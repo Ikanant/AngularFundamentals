b009c4e28997a8b2e7212c66a7c7c558aaf8c05a
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2016-09-04 01:35:12 -0500

    Added new custom directive to a sample page

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,2 +1,10 @@
 'use strict';
 
+eventsApp.directive('myDirective', function($compile){
+  return {
+    link: function (scope, element, attrs, controller) {
+      var markup = '<input type="text" name="apple" ng-model="test"/> <h1>{{test}}</h1>';
+      angular.element(element).html($compile(markup)(scope));
+    }
+  }
+});
