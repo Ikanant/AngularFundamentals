95f3cc539202a8a3db50c18c6084eb2a3714a627
Ikanant <jonathan.hdez92@gmail.com>
2016-09-11 01:35:12 -0500

    Added hide/show functionality to event details page

--- app/js/directives/collapsible.js
+++ app/js/directives/collapsible.js
@@ -4,10 +4,17 @@ eventsApp.directive('collapsible', function() {
   return {
     restrict: 'E',
     replace: true,
-    template: '<div><h4 class="well-title">{{title}}</h4><div ng-transclude></div></div>',
+    template: '<div><h4 class="well-title" ng-click="toggleVisibility()">{{title}}</h4><div ng-show="visible" ng-transclude></div></div>',
     transclude: true,
     scope: {
       title: '@'
+    },
+    controller: function($scope){
+      $scope.visible = true;
+
+      $scope.toggleVisibility = function(){
+        $scope.visible = !$scope.visible;
+      }
     }
   }
 });
